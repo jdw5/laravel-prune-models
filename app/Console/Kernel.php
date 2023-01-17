bd347a02ceb0adb5eaa0bd81811d2e597542be98
jdw5 <joshuadwallace1@gmail.com>
2023-01-17 13:23:21 +1000

    Scheduling of prune command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\Event;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('model:prune', [
+            '--model' => [
+                Event::class,
+            ]
+        ])->weekly();
+
+        // $schedule->command('model:prune', [
+        //     '--model' => [
+        //         User::class,
+        //     ]
+        // ])->weekends();
     }
 
     /**
@@ -25,6 +36,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
+
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
