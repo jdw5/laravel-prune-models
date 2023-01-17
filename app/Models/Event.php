5c602b2c576fe2869bc372906ec29f5a3917667a
jdw5 <joshuadwallace1@gmail.com>
2023-01-17 13:29:16 +1000

    Pruning methods for cleaning up relations

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -12,8 +12,25 @@ class Event extends Model
     use HasFactory,
         Prunable;
 
+    /*
+        Prunable method will provide the condition of which models to delete
+    */
     public function prunable() : Builder
     {
         return static::where('created_at', '<=', now()->subHour(4));
+    }
+
+    /*
+        Pruning method will run on each instance of the method being pruned.
+        Use for foreign ID constraints to remove them, alternatively implement onDelete('cascade') at DB level
+    */
+    public function pruning()
+    {
+        $this->deploymentLogs->delete();
+    }
+
+    public function eventLogs()
+    {
+        return $this->hasMany(EventLog::class);
     }
 }
